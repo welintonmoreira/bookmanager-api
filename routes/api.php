90886b8ceba2c526c290217b492f1712c25e7ed0
Welinton Moreira <dev.welintonmoreira@gmail.com>
2023-06-11 18:03:11 -0300

    Rotas CRUD de autor.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Author (Other routes).
+Route::prefix('author')->group(function () {
+    Route::controller(AuthorController::class)->group(function () {
+    });
+});
+
+// Author (CRUD routes).
+Route::resource('author', AuthorController::class);
