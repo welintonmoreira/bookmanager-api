22becd65a7707083ee04e4b479b4c250424f6412
Welinton Moreira <welinton.moreira@unyleya.com.br>
2025-06-27 23:10:20 -0300

    Retornando método deletado.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    /**Add commentMore actions
+     * @return bool
+     */
+    public static function isShowExceptionMessage(): bool
+    {
+        return (bool)env('SHOW_EXCEPTION_MESSAGE');
+    }
 }
